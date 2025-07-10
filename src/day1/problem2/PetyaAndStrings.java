17caf87f1b1d1fd36bbc37a3e1684583c4acb1b8
GargKrishna24 <gargkrishna@888@gmail.com>
2025-07-10 19:29:56 +0530

    Add . day 2 problems

--- src/day1/problem2/PetyaAndStrings.java
+++ src/day1/problem2/PetyaAndStrings.java
@@ -1,5 +1,17 @@
 package day1.problem2;
 
-public class PetyaAndStrings {
+import java.util.Scanner;
 
+public class PetyaAndStrings {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String firstGift = scanner.nextLine(), secondGift = scanner.nextLine();
+        firstGift = firstGift.toLowerCase();
+        secondGift=secondGift.toLowerCase();
+        int result = firstGift.compareTo(secondGift);
+        if(result == 0 ) System.out.println(0);
+        else if (result > 0 ) System.out.println(1);
+        else  System.out.println(0);
+        scanner.close();
+    }
 }
